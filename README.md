25e3c0fbdf0c303225fd03529a1af8ecbd56d5fd
OliverMarcusson <oliver.marcusson@elev.ga.ntig.se>
2022-06-17 18:48:15 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Min Personliga Hemsida!
---
+++ index.js
@@ -0,0 +1,7 @@
+function removeNotification(element) {
+  var notification = element.parentElement;
+  console.log(notification);
+  notification.style.display = "none";
+}
+
+console.log("Hello world!");
